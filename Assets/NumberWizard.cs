ac59285a591b08fb36f1b712f9b65966975aeb08
danielmartin0126 <danielmartin0126@gmail.com>
2020-05-20 13:49:56 -0400

    Test commit
    First real commit using unity github plugin

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int max = 1000;
+        int min = 1;
+        Debug.Log("Welcome to the show");
+        Debug.Log("Pick a number between " + min + " and " + max);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
