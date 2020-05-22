a6514d2c74af1d42ba8d61358d586e4c403e7ff1
danielmartin0126 <danielmartin0126@gmail.com>
2020-05-21 21:00:32 -0400

    core guessing functionality
    conditionals for changing guess value

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -4,18 +4,43 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour
 {
+    int max = 1000;
+    int min = 1;
+    int guess = (1000 + 1) / 2;
+
     // Start is called before the first frame update
     void Start()
     {
-        int max = 1000;
-        int min = 1;
         Debug.Log("Welcome to the show");
         Debug.Log("Pick a number between " + min + " and " + max);
+        Debug.Log("Tell me if your number is higher or lower than");
+        Debug.Log("Push up if higher");
+        Debug.Log("Push down if lower");
+        Debug.Log("Push enter if correct");
+        Debug.Log("Is your number higher or lower than " + guess + "?");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow)){
+            Debug.Log("Up arrow pressed");
+            min = guess;
+            //Debug.Log("UPDATED MIN" + guess);
+            guess = (max + min) / 2;
+            Debug.Log("Is your number higher or lower than " + guess + "?");
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("Down arrow pressed");
+            max = guess;
+            //Debug.Log("UPDATED MAX" + max);
+            guess = (max + min) / 2;
+            Debug.Log("Is your number higher or lower than " + guess + "?");
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("NEAT. YOUR NUMBER WAS " + guess);
+        }
     }
 }
